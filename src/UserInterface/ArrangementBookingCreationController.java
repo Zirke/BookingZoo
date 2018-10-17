efe6bf46af25ec1bca1bb795727593446c8febc5
TheKaergaard <31920988+TheKaergaard@users.noreply.github.com>
2018-10-18 01:09:54 +0200

    GUI update

--- src/UserInterface/ArrangementBookingCreationController.java
+++ src/UserInterface/ArrangementBookingCreationController.java
@@ -1,12 +1,42 @@
 package UserInterface;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
-public class ArrangementBookingCreationController {
+public class ArrangementBookingCreationController extends GeneralController {
     @FXML
-    private Button cancelButton;
+    private DatePicker datePicker;
+
+    @FXML
+    private ToggleGroup timeGroup, participantGroup, menuGroup;
+
+    @FXML
+    private TextField noOfChildrenTextField, childNameTextField, childAgeTextField, contactPersonTextField,
+            phoneNumberTextField, emailTextField;
+    @FXML
+    private TextArea customerCommentTextField, commentTextField;
+
+    @FXML
+    private Button createAndCloseButton, cancelButton;
+
+    @FXML
+    public void createArrangementBookingFromInput() {
+        String date = datePicker.getValue().toString();
+        String time = timeGroup.getSelectedToggle().toString();
+        String noOfChildren = noOfChildrenTextField.getText();
+        String childName = childNameTextField.getText();
+        String childAge = childAgeTextField.getText();
+        String contactPerson = contactPersonTextField.getText();
+        String phoneNumber = phoneNumberTextField.getText();
+        String email = emailTextField.getText();
+        String participant = participantGroup.getSelectedToggle().toString();
+        String menuChoice = menuGroup.getSelectedToggle().toString();
+
+        System.out.println("Booking created:\n" + date + time + noOfChildren + childName + childAge + contactPerson + phoneNumber + email + participant + menuChoice);
+
+        showAlertBox(Alert.AlertType.CONFIRMATION, "", "Er den indtastede information korrekt?");
+    }
 
     @FXML
     public void closeWindow() {
