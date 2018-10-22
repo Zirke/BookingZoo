9aec8100bcf89ba919dc64334ef5dfccc3ecd525
Zirke <defrenefre@gmail.com>
2018-10-22 13:37:36 +0200

    -

--- src/UserInterface/EditArrangementBookingController.java
+++ src/UserInterface/EditArrangementBookingController.java
@@ -1,9 +1,19 @@
 package UserInterface;
 
 import Bookings.ArrangementBooking;
+import Bookings.BookingDataAccessor;
+import Bookings.FoodOrder;
 import Customers.Customer;
+import enums.BookingStatus;
+import enums.ChoiceOfMenu;
+import enums.FacilityState;
+import facilities.Restaurant;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import java.sql.SQLException;
+import java.util.Optional;
 
 public class EditArrangementBookingController {
     private ArrangementBooking selectedArrangementBooking;
@@ -25,9 +35,50 @@ public class EditArrangementBookingController {
     @FXML
     private ChoiceBox categoryChoiceBox;
     @FXML
+    private Button cancelButton, saveAndCloseButton;
+    @FXML
     private TextArea customerCommentTextArea, commentTextArea;
 
-    public void initialize() {
+    public void initialize() throws SQLException, ClassNotFoundException {
+
+        BookingDataAccessor bda = new BookingDataAccessor(
+                "org.postgresql.Driver",
+                "jdbc:postgresql://packy.db.elephantsql.com/jyjczxth",
+                "jyjczxth",
+                "nw51BNKhctporjIFT5Qhhm72jwGVJK95");
+
+        categoryChoiceBox.getItems().addAll("Afventende","Aktiv","Færdig","Arkiveret");
+
+        saveAndCloseButton.setOnMouseClicked(e -> {
+            Alert alert2 = new Alert(Alert.AlertType.CONFIRMATION);
+            alert2.setContentText("Er den indtastede information korrekt?");
+
+            Optional<ButtonType> alertChoice2 = alert2.showAndWait();
+
+            if (alertChoice2.get() == ButtonType.OK) {
+                try {
+                    bda.editArrBook(overwriteSelectedArrangementBooking());
+                    closeWindow();
+                } catch (SQLException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        });
+
+        noOfChildrenTextField.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.matches("\\d*")) {
+                noOfChildrenTextField.setText(newValue.replaceAll("[^\\d]", ""));
+            }
+        });
+
+        childAgeTextField.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.matches("\\d*")) {
+                childAgeTextField.setText(newValue.replaceAll("[^\\d]", ""));
+            }
+        });
+        cancelButton.setOnMouseClicked(e -> closeWindow());
+
+
 
     }
 
@@ -73,5 +124,57 @@ public class EditArrangementBookingController {
                 menuFourRadioButton.setSelected(true);
                 break;
         }
+        customerCommentTextArea.setText(selectedArrangementBooking.getCustomerComment());
+        commentTextArea.setText(selectedArrangementBooking.getComment());
+
+    }
+
+    private ArrangementBooking overwriteSelectedArrangementBooking() {
+        BookingStatus statusChoice;
+        statusChoice = BookingStatus.statusChosen(categoryChoiceBox.getSelectionModel().getSelectedItem().toString());
+        selectedArrangementBooking.setBookingStatus(statusChoice);
+        selectedArrangementBooking.setDate(datePicker.getValue());
+        RadioButton selectedTimeSlot = (RadioButton) timeGroup.getSelectedToggle();
+        selectedArrangementBooking.setTime(selectedTimeSlot.getText());
+        selectedArrangementBooking.setParticipants(Integer.parseInt(noOfChildrenTextField.getText()));
+        selectedArrangementBooking.setCustomerComment(customerCommentTextArea.getText());
+        selectedArrangementBooking.setComment(commentTextArea.getText());
+        ChoiceOfMenu menuChoice = null;
+        RadioButton menuChoiceButton = (RadioButton) menuGroup.getSelectedToggle();
+        switch (menuChoiceButton.getText()) {
+            case "Kakao, boller og kage":
+                menuChoice = ChoiceOfMenu.MENU_ONE;
+                break;
+            case "Frikadeller og dessert":
+                menuChoice = ChoiceOfMenu.MENU_TWO;
+                break;
+            case "Pasta m. kødsovs og dessert":
+                menuChoice = ChoiceOfMenu.MENU_THREE;
+                break;
+            case "Nuggets og pommes frites":
+                menuChoice = ChoiceOfMenu.MENU_FOUR;
+                break;
+            case "Ingen mad":
+                menuChoice = ChoiceOfMenu.NO_FOOD;
+                break;
+        }
+        selectedArrangementBooking.setMenuChosen(new FoodOrder(menuChoice));
+        selectedArrangementBooking.setRestaurant(new Restaurant(FacilityState.OCCUPIED));
+        selectedArrangementBooking.setBirthdayChildName(childNameTextField.getText());
+        selectedArrangementBooking.setBirthdayChildAge(Integer.parseInt(childAgeTextField.getText()));
+        RadioButton formerParticipant = (RadioButton) participantGroup.getSelectedToggle();
+        selectedArrangementBooking.setFormerParticipant(formerParticipant.getText());
+        selectedArrangementBooking.setGuide(guideTextField.getText());
+        Customer temp = new Customer(contactPersonTextField.getText(), phoneNumberTextField.getText(),
+                emailTextField.getText());
+        selectedArrangementBooking.setCustomer(temp);
+
+
+        return selectedArrangementBooking;
+    }
+
+    private void closeWindow() {
+        Stage stage = (Stage) cancelButton.getScene().getWindow();
+        stage.close();
     }
 }
