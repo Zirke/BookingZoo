ce20f1482deb8f8a14e0b163ee2a2ad40bd9cf31
TheKaergaard <31920988+TheKaergaard@users.noreply.github.com>
2018-10-30 14:03:38 +0100

    ...

--- src/enums/LectureRoomType.java
+++ src/enums/LectureRoomType.java
@@ -12,6 +12,12 @@ public enum LectureRoomType {
         public String toString() {
             return "Biologisk lokale";
         }
+    },
+    NOT_CHOSEN {
+        @Override
+        public String toString() {
+            return "Intet lokale valgt";
+        }
     };
 
     public static LectureRoomType roomTypeChoice(String input) {
@@ -21,6 +27,8 @@ public enum LectureRoomType {
                 return SAVANNAH_TYPE;
             case "Biologisk lokale":
                 return BIOLOGICAL_TYPE;
+            case "Intet lokale valgt":
+                return NOT_CHOSEN;
             default:
                 throw new IllegalArgumentException();
         }
